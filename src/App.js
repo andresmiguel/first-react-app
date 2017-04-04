80234e376c38429f81f2f69fae077793c5369de8
Andrés Miguel Bores Hevia <andresmbores@gmail.com>
2017-04-04 00:04:40 -0300

    Changed App style and content. Added module.hot.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const helloWorld = 'Welcome to React!',
+        message = 'My first App with React!';
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>{helloWorld}</h2>
+          <h3>{message}</h3>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
